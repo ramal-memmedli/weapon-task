125ed53c8dc220389bfec6441c31554c9197324c
Ramal Məmmədli <memmedliramal00@gmail.com>
2022-03-22 17:27:39 +0400

    Weapon Task project added.

---
+++ WeaponTask/WeaponTask/Models/Weapon.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeaponTask.Models
+{
+    public class Weapon
+    {
+        public Weapon(int magazineSize, int currentBulletPoint, bool shootMode)
+        {
+            MagazineSize = magazineSize;
+            CurrentBulletCount = currentBulletPoint;
+            ShootMode = shootMode;
+        }
+
+        public int MagazineSize { get { return _magazineSize; } set { if (value > 0 && value < 100000) _magazineSize = value; } }
+        public int CurrentBulletCount { get { return _currentBulletCount; } set { if (value >= 0 && value <= _magazineSize) _currentBulletCount = value; } }
+        public bool ShootMode { get; set; }
+
+
+        private int _magazineSize;
+        private int _currentBulletCount;
+
+        public void Shoot()
+        {
+            if(CurrentBulletCount > 0)
+            {
+                --CurrentBulletCount;
+                Console.WriteLine("Fired");
+            }
+            else
+            {
+                Console.WriteLine("Bullet ran out");
+            }
+        }
+        public void Fire()
+        {
+            if (CurrentBulletCount > 0)
+            {
+                if (ShootMode == true)
+                {
+                    --CurrentBulletCount;
+                    Console.WriteLine("One bullet fired");
+                }
+                else
+                {
+                    CurrentBulletCount = 0;
+                    Console.WriteLine("All Bullets Fired");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Bullet ran out");
+            }
+        }
+        public int GetRemainBulletCount()
+        {
+            Console.WriteLine(MagazineSize - CurrentBulletCount + " " + "Current bullet count : " + CurrentBulletCount);
+            return MagazineSize - CurrentBulletCount;
+        }
+        public void Reload()
+        {
+            if(CurrentBulletCount < MagazineSize)
+            {
+                CurrentBulletCount = MagazineSize;
+                Console.WriteLine("Weapon reloaded");
+            }
+            else
+            {
+                Console.WriteLine("Weapon already reloaded");
+            }
+        }
+        public void ChangeFireMode()
+        {
+            if(ShootMode == true)
+            {
+                ShootMode = false;
+            }
+            else if(ShootMode == false)
+            {
+                ShootMode = true;
+            }
+            Console.WriteLine("Weapon fire mode changed");
+        }
+
+        public void EditMagazineSize(int newMagazineSize)
+        {
+            MagazineSize = newMagazineSize;
+            Console.WriteLine($"Magazine size updated to {MagazineSize}");
+        }
+
+        public void EditBulletCount(int newBulletCount)
+        {
+            CurrentBulletCount = newBulletCount;
+            Console.WriteLine($"Current bullet count updated to {CurrentBulletCount}");
+        }
+    }
+}
---
+++ WeaponTask/WeaponTask/Program.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using WeaponTask.Models;
+
+namespace WeaponTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        ReEnterMagazineSize:
+            Console.Write("Enter Magazine Size : ");
+            int magazineSize = Convert.ToInt32(Console.ReadLine());
+
+            if (magazineSize > 0 && magazineSize < 100000)
+            {
+
+            }
+            else
+            {
+                goto ReEnterMagazineSize;
+            }
+
+        ReEnterRemainBullet:
+            Console.Write("Enter Remain Bullet Count : ");
+
+
+            int remainBulletCount = Convert.ToInt32(Console.ReadLine());
+            if (remainBulletCount <= magazineSize && remainBulletCount >= 0)
+            {
+
+            }
+            else
+            {
+                goto ReEnterRemainBullet;
+            }
+
+            Console.Write("Enter default fire mode (1 - single, 2 - auto) : ");
+            int defaultFireMode = Convert.ToInt32(Console.ReadLine());
+            bool isSingle = true;
+            switch (defaultFireMode)
+            {
+                case 1:
+                    isSingle = true;
+                    break;
+                case 2:
+                    isSingle = false;
+                    break;
+            }
+
+            Weapon weapon = new Weapon(magazineSize, remainBulletCount, isSingle);
+
+
+            int choice;
+            Console.WriteLine("0 - Get information\n1 - Shoot\n2 - Fire\n3 - Remain bullet count\n4 - Reload weapon\n5 - Change fire mode\n6 - exit\n7 - Edit");
+            Console.WriteLine("Enter 6 for exit.");
+            do
+            {
+                Console.Write("Choose the command : ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("0 - Get information\n1 - Shoot\n2 - Fire\n3 - Remain bullet count\n4 - Reload weapon\n5 - Change fire mode\n6 - exit\n7 - Edit");
+                        break;
+                    case 1:
+                        Console.WriteLine("------------------------");
+                        weapon.Shoot();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 2:
+                        Console.WriteLine("------------------------");
+                        weapon.Fire();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 3:
+                        Console.WriteLine("------------------------");
+                        weapon.GetRemainBulletCount();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 4:
+                        Console.WriteLine("------------------------");
+                        weapon.Reload();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 5:
+                        Console.WriteLine("------------------------");
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("------------------------");
+                        break;
+                    case 7:
+                        Console.WriteLine("Edit :\n8 - Edit magazine size\n9 - Edit current bullet count");
+                        break;
+                    case 8:
+                    ReEnterNewMagazineSize:
+                        Console.Write("Enter magazine size : ");
+                        int newMagazineSize = Convert.ToInt32(Console.ReadLine());
+                        if (newMagazineSize > weapon.CurrentBulletCount)
+                        {
+                            weapon.EditMagazineSize(newMagazineSize);
+                        }
+                        else
+                        {
+                            goto ReEnterNewMagazineSize;
+                        }
+                        break;
+                    case 9:
+                    ReEnterNewCurrentBulletCount:
+                        Console.Write("Enter current bullet count : ");
+                        int newCurrentBulletCount = Convert.ToInt32(Console.ReadLine());
+                        if (newCurrentBulletCount >= 0 && newCurrentBulletCount < weapon.MagazineSize)
+                        {
+                            weapon.EditBulletCount(newCurrentBulletCount);
+                        }
+                        else
+                        {
+                            goto ReEnterNewCurrentBulletCount;
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (choice != 6);
+        }
+    }
+}
